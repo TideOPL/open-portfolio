2b89d24346ab2bef49a1f0603a30980bbd606a6c
Tide <stargamerminer@gmail.com>
2023-07-21 23:19:02 +0100

    lint

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return(
-        <div className="sm:px-10 md:px-20 font-mono text-center h-24 dark:text-white ">
+        <div className="px-3 md:px-20 font-mono text-center  text-xs sm:h-24 md:h-12 dark:text-white ">
             <div>
                 Designed with 💜 and written in Visual Studio Code by Obi Newman. Built with Next.js & Tailwind CSS, deployed with Vercel. Text written with Monospace.
             </div>
